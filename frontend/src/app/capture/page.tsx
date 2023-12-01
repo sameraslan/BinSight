5d3267b3d2fa9a4b49c3a9050183c4e80119d40b
sameraslan <82460915+sameraslan@users.noreply.github.com>
2023-12-01 18:29:33 -0500

    Fixed capture

--- frontend/src/app/capture/page.tsx
+++ frontend/src/app/capture/page.tsx
@@ -1,129 +1,107 @@
 'use client'
-import { useState, useEffect, useRef } from 'react';
-import Webcam from 'react-webcam';
+import React, { useRef, useState, useEffect } from 'react';
+import { Box, Center, VStack, Text, useTheme } from '@chakra-ui/react';
 import * as tf from '@tensorflow/tfjs';
-import { loadYoloV8n } from './model';
-import { drawBoxes } from './drawing';
-import { nonMaxSuppression } from './nms';
+import '@tensorflow/tfjs-backend-webgl';
+import { Webcam } from './webcam'; // Update the path as per your project structure
+import { renderBoxes } from './renderBox';
+import { non_max_suppression } from './nonMaxSuppression';
 
-const TRASH_CLASSES = [39, 40, 41, 42, 43, 44, 45]; //bottle, cup etc
+export default function Home() {
+    const videoRef = useRef(null);
+    const canvasRef = useRef(null);
+    const theme = useTheme();
+    const [loading, setLoading] = useState({ loading: true, progress: 0 });
+    const modelName = 'yolov7';
 
-function TrashDetection() {
-  const webcamRef = useRef(null);
-  const canvasRef = useRef(null);
-  
-  const [model, setModel] = useState(null);
-  const [capturing, setCapturing] = useState(false);
-  const [captureCount, setCaptureCount] = useState(0);
-  const [detectedBox, setDetectedBox] = useState(null);
-  
-  useEffect(() => {
-    async function loadModel() {
-      const model = await loadYoloV8n();
-      setModel(model);
-    }
-    loadModel();
-  }, []);
-
-  const detectFrame = async () => {
-    const video = webcamRef.current;
-    const canvas = canvasRef.current;
+    // Function to detect objects in each frame
+    const detectFrame = async (model, videoRef, canvasRef) => {
+        const videoElement = videoRef.current;
+        const canvasElement = canvasRef.current;
+        if (videoElement && canvasElement) {
+            canvasElement.width = videoElement.clientWidth;
+            canvasElement.height = videoElement.clientHeight;
+        }
+        const model_dim = [480, 640];
+        tf.engine().startScope();
+        const input = tf.tidy(() => {
+            let img = tf.browser.fromPixels(videoRef.current)
+                        .resizeNearestNeighbor([model_dim[1], model_dim[0]]) // Resize to 640x480
     
-    const [videoWidth, videoHeight] = [video.videoWidth, video.videoHeight];
+            // Calculate padding
+            const padWidth = (model_dim[1] - model_dim[0]) / 2;
+            const padding = [[0, 0], [padWidth, padWidth], [0, 0]];
     
-    // Define ROI 
-    const roiWidth = videoWidth / 2; 
-    const roiHeight = videoHeight / 2;
-    const roiX = (videoWidth - roiWidth) / 2;
-    const roiY = (videoHeight - roiHeight) / 2;
-
-    const frame = tf.tidy(() => {
-      // Capture frame from video
-      let img = tf.browser.fromPixels(video);
-
-      // Crop ROI  
-      img = img.slice([roiY, roiX, 0], [roiHeight, roiWidth, 3]); 
-
-      // Preprocess
-      img = img.div(255.0);
-      // Add batch dimension
-      img = img.expandDims(0); 
-      return img;
-    });
+            // Pad the image to make it square
+            let paddedImg = img.pad(padding, 0); // Pad with zeros (black)
     
-    // Detect objects
-    const results = await model.execute(frame);
+            paddedImg = paddedImg.div(255.0)
+                                .transpose([2, 0, 1])
+                                .expandDims(0);
     
-    // Postprocess detections
-    const [boxes, scores, classes] = tf.split(results, [4, 1, 1], 2);
-    const indices = tf.where(scores > 0.5);  
-    const selectedBoxes = tf.gather_nd(boxes, indices);
-    const selectedScores = tf.gather_nd(scores, indices);
-    const selectedClasses = tf.gather_nd(classes, indices);
+            return paddedImg;
+        });
 
-    // Run NMS    
-    const detections = nonMaxSuppression(
-      selectedBoxes.arraySync(), 
-      selectedScores.arraySync(),
-      selectedClasses.arraySync()
+        await model.executeAsync(input).then((res) => {
+            res = res.arraySync()[0];
+            var detections = non_max_suppression(res);
+            const boxes = detections.map(d => d.slice(0, 4));
+            const scores = detections.map(d => d[4]);
+            const classes = detections.map(d => d[5]);
+
+            renderBoxes(canvasRef, 0.80, boxes, scores, classes);
+            tf.dispose(res);
+        });
+
+        requestAnimationFrame(() => detectFrame(model, videoRef, canvasRef));
+        tf.engine().endScope();
+    };
+
+    // Effect hook to load the YOLOv7 model
+    useEffect(() => {
+        const webcam = new Webcam();
+
+        tf.loadGraphModel(`${window.location.origin}/${modelName}_web_model/model.json`, {
+            onProgress: (fractions) => {
+                setLoading({ loading: true, progress: fractions });
+            },
+        }).then(async (yolov7) => {
+            // Warmup the model before using real data
+            const dummyInput = tf.ones(yolov7.inputs[0].shape);
+            await yolov7.executeAsync(dummyInput).then((warmupResult) => {
+                tf.dispose(warmupResult);
+                tf.dispose(dummyInput);
+
+                setLoading({ loading: false, progress: 1 });
+                webcam.open(videoRef, () => detectFrame(yolov7, videoRef, canvasRef));
+            });
+        });
+    }, []);
+
+    return (
+        <Center h="100vh">
+            <VStack spacing={4} align="stretch">
+                <Text fontSize="2xl" fontWeight="bold" textAlign="center">
+                    Hi! Place your item in the box
+                </Text>
+                <Box
+                    display="flex"
+                    justifyContent="center"
+                    alignItems="center"
+                    w="full"
+                    h="auto"
+                    position="relative"
+                    bg={theme.colors.primary[500]}
+                    p={8}
+                    borderRadius="xl"
+                >
+                    {loading.loading ? (
+                        <div>Loading model... {Math.floor(loading.progress * 100)}%</div>
+                    ) : null}
+                    <video ref={videoRef} style={{ width: '640px', height: '480px' }} autoPlay playsInline muted />
+                    <canvas ref={canvasRef} style={{ position: 'absolute', width: '640px', height: '480px' }} />
+                </Box>
+            </VStack>
+        </Center>
     );
-
-    // Draw boxes
-    drawBoxes(canvas, detections, TRASH_CLASSES); 
-
-    tf.dispose(frame);
-    tf.dispose(results);
-    tf.dispose(selectedBoxes);
-    tf.dispose(selectedScores);
-    tf.dispose(selectedClasses);
-
-    if(capturing) {
-      // Check if trash detected
-      if(detections.length > 0) {
-        setCaptureCount(count => count + 1);  
-      }
-
-      // Save image if trash is stable
-      if(captureCount > 15) {
-        setDetectedBox(detections[0]); 
-        setCapturing(false);
-      } 
-    }
-
-    requestAnimationFrame(() => detectFrame());
-  }
-
-  useEffect(() => {
-    if(!model) {
-      return;
-    }
-    detectFrame();
-  }, [model]);
-
-  const handleCapture = () => {
-    setCapturing(true);
-    setCaptureCount(0);
-    setDetectedBox(null);
-  }
-
-  // Triggered when stable trash detected
-  const handleSave = () => {
-    // Code to save cropped image using box coords    
-  }
-
-  return (
-    <div>
-      <Webcam ref={webcamRef} />
-      <canvas ref={canvasRef} />
-
-      <button onClick={handleCapture}>Capture Trash</button>  
-      {detectedBox && (
-        <button onClick={handleSave}>
-          Save {detectedBox.className}
-        </button>  
-      )}
-    </div>
-  );
 }
-
-export default TrashDetection;
